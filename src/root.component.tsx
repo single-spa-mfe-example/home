4e7c5a31b2c8a3f448f766f7880e1413c1b70ef5
Corwin Marsh <corwinm@users.noreply.github.com>
2022-05-09 15:02:41 -0700

    Add gh-pages deploy

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,19 @@
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <main>
+      <section className="hero is-primary is-fullheight-with-navbar">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <p className="title">Example App</p>
+            <p className="subtitle">
+              Microfrontends with{" "}
+              <a href="https://single-spa.js.org/" rel="noopener">
+                Single-Spa
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
